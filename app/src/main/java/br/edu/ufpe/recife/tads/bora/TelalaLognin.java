2c08b1639d7f6ac97c45327fa1b7050e2d65ea09
Wsanbey <welrysanbey@hotmail.com>
2022-06-01 00:45:27 -0300

    adicionado fonte, tela inicial, pasta de fontes.

---
+++ app/src/main/java/br/edu/ufpe/recife/tads/bora/TelalaLognin.java
@@ -0,0 +1,14 @@
+package br.edu.ufpe.recife.tads.bora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelalaLognin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telala_lognin);
+    }
+}
